6e86ea22d6553ff1b6f96c760b1c5e35d3948f8a
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-03-10 12:28:20 -0300

    Adicionando estudo sobre polimorfismo

---
+++ src/javaIntermediario/polimorfismo/Main.java
@@ -0,0 +1,15 @@
+package javaIntermediario.polimorfismo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Uzumaki naruto = new Uzumaki();
+        naruto.nome = "Naruto Uzumaki";
+        naruto.habilidadeEspecial();
+
+        Uchiha sasuuke = new Uchiha();
+        sasuuke.nome = "Sasuke Uchiha";
+        sasuuke.habilidadeEspecial();
+        
+    }
+}
---
+++ src/javaIntermediario/polimorfismo/Ninja.java
@@ -0,0 +1,13 @@
+package javaIntermediario.polimorfismo;
+
+public class Ninja {
+
+    String nome;
+    int idade;
+    String aldeia;
+
+    // Metodo geral! Todos os ninjas vão ter
+    public void habilidadeEspecial() {
+        System.out.println("Meu nome é " + nome + " e esse é meu ataque especial");
+    }
+}
