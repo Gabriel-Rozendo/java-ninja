c11a25b36106ff152c5a11c0ad72ac35a42bb7a3
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-03-21 12:37:59 -0300

    feat: Adiciona estudo sobre métodos final

---
+++ src/javaIntermediario/finalMethods/Uzumaki.java
@@ -0,0 +1,11 @@
+package javaIntermediario.finalMethods;
+
+public class Uzumaki extends Ninja {
+
+    public Uzumaki(String nome, String aldeia, int idade) {
+        super(nome, aldeia, idade);
+    }
+
+    public Uzumaki() {
+    }
+}
