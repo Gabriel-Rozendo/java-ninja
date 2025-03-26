bc56c0924ac554212b366580082e3b0f632af355
Gabriel <gabrielrozendodacosta@gmail.com>
2025-03-25 22:20:12 -0300

    feat: Adiciona estudo sobre encapsulamento

---
+++ src/javaIntermediario/encapsulamento/Uzumaki.java
@@ -0,0 +1,11 @@
+package javaIntermediario.encapsulamento;
+
+public class Uzumaki extends Ninja{
+
+    public Uzumaki(String nome, String aldeia, int idade, int numeroMissoesConcluidas, double altura) {
+        super(nome, aldeia, idade, numeroMissoesConcluidas, altura);
+    }
+
+    public Uzumaki() {
+    }
+}
